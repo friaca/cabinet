843c8a5ecf62ecc75dc37594a4ad180a3ecd2c1a
friaca <friacajoao@gmail.com>
2023-02-20 22:40:17 -0300

    refact: creating/updating a Transaction

--- lib/cabinet/warehouse.ex
+++ lib/cabinet/warehouse.ex
@@ -4,6 +4,7 @@ defmodule Cabinet.Warehouse do
   """
 
   import Ecto.Query, warn: false
+  alias Ecto.Repo.Transaction
   alias Cabinet.Repo
 
   alias Cabinet.Warehouse.Product
@@ -147,20 +148,18 @@ defmodule Cabinet.Warehouse do
   """
   def create_transaction(attrs \\ %{}) do
     transaction = %Transaction{}
-      |> Transaction.changeset(attrs)
-      |> Repo.insert()
+    |> Transaction.changeset(attrs)
 
-    product = Map.get(attrs, "product_id") |> get_product!()
-    {amount, _} = Map.get(attrs, "amount") |> Decimal.parse()
+    product = Product.get_product_changeset_by_transaction(Map.get(attrs, "amount"), Map.get(attrs, "product_id"))
 
-    case Map.fetch!(product, :list_by) do
-      :quantity ->
-        product |> update_product(%{quantity: Decimal.add(product.quantity, amount)})
-      :weight ->
-        product |> update_product(%{weight: Decimal.add(product.weight, amount)})
+    Ecto.Multi.new()
+    |> Ecto.Multi.insert(:transaction, transaction)
+    |> Ecto.Multi.update(:product, product)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{transaction: transaction, product: _product}} -> {:ok, transaction}
+      {:error, error} -> Repo.rollback(error)
     end
-
-    transaction
   end
 
   @doc """
@@ -176,26 +175,20 @@ defmodule Cabinet.Warehouse do
 
   """
   def update_transaction(%Transaction{} = transaction, attrs) do
-    product = Map.get(attrs, "product_id") |> get_product!()
-    {op, difference} = case { Map.get(transaction, :amount), Map.get(attrs, "amount") } do
-      {prev, curr} when prev > curr -> {:sub, Decimal.sub(prev, curr)}
-      {prev, curr} when prev < curr -> {:add, Decimal.sub(curr, prev)}
-    end
-
-    case {op, Map.fetch!(product, :list_by)} do
-      {:add, :quantity} ->
-        product |> update_product(%{quantity: Decimal.add(product.quantity, difference)})
-      {:sub, :quantity} ->
-        product |> update_product(%{quantity: Decimal.sub(product.quantity, difference)})
-      {:add, :weight} ->
-        product |> update_product(%{weight: Decimal.add(product.weight, difference)})
-      {:sub, :weight} ->
-        product |> update_product(%{weight: Decimal.sub(product.weight, difference)})
-    end
-
-    transaction
+    transaction_changeset = transaction
     |> Transaction.changeset(attrs)
-    |> Repo.update()
+
+    amount_difference = Transaction.get_amount_difference(Map.get(transaction, :amount), Map.get(attrs, "amount"))
+    product = Product.get_product_changeset_by_transaction(amount_difference, Map.get(attrs, "product_id"))
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:transaction, transaction_changeset)
+    |> Ecto.Multi.update(:product, product)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{transaction: transaction, product: _product}} -> {:ok, transaction}
+      {:error, error} -> Repo.rollback(error)
+    end
   end
 
   @doc """
