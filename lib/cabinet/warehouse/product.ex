aff538b5c4b451d551d2e3132c014d015e5d9ad8
friaca <friacajoao@gmail.com>
2023-09-10 17:59:20 -0300

    feat: helper product functions for transactions

--- lib/cabinet/warehouse/product.ex
+++ lib/cabinet/warehouse/product.ex
@@ -14,20 +14,20 @@ defmodule Cabinet.Warehouse.Product do
     timestamps()
   end
 
-  defp enum_mappings do
-    %{
-      :list_by => %{:weight => "Peso", :quantity => "Quantidade"},
-      :type => %{:raw => "Material", :final => "Final"}
-    }
-  end
-
   def translate_enum(field, value) do
     enum_mappings()[field][value]
   end
 
   def select_options(field, form) do
     Enum.reduce(enum_mappings()[field], [], fn {enum, translation}, acc ->
-      [[key: to_string(translation), value: enum, selected: Map.fetch!(form.data, field) == translation] | acc]
+      [
+        [
+          key: to_string(translation),
+          value: enum,
+          selected: Map.fetch!(form.data, field) == translation
+        ]
+        | acc
+      ]
     end)
   end
 
@@ -35,6 +35,51 @@ defmodule Cabinet.Warehouse.Product do
   def changeset(product, attrs) do
     product
     |> cast(attrs, [:name, :type, :weight, :quantity, :list_by])
-    |> validate_required([:name, :type, :list_by])
+    |> validate_required([:name, :type, :list_by], message: "Não pode ficar em branco.")
+    |> validate_listing()
+  end
+
+  def cast_listing(product, value) when is_binary(value) do
+    {decimal, _} = Decimal.parse(value)
+    cast_listing(product, decimal)
+  end
+
+  def cast_listing(product, value) do
+    if cast_to_integer?(product) do
+      Decimal.to_integer(value)
+    else
+      value
+    end
+  end
+
+  def get_changeset_by_transaction(transaction_amount, product_id) do
+    product = Cabinet.Warehouse.get_product!(product_id)
+    {field, difference} = get_product_difference(transaction_amount, product)
+
+    product
+    |> Ecto.Changeset.cast(%{field => difference}, [field])
+  end
+
+  defp cast_to_integer?(product), do: Map.get(product, :list_by) == :quantity
+
+  defp validate_listing(changeset) do
+    case get_field(changeset, :list_by) do
+      nil -> changeset
+      field -> changeset |> validate_required(field, message: "Não pode ficar em branco.")
+    end
+  end
+
+  defp get_product_difference(transaction_amount, product) do
+    list_by = Map.get(product, :list_by)
+    transaction_amount_cast = product |> cast_listing(transaction_amount)
+
+    {list_by, Map.get(product, list_by) + transaction_amount_cast}
+  end
+
+  defp enum_mappings do
+    %{
+      :list_by => %{:weight => "Peso", :quantity => "Quantidade"},
+      :type => %{:raw => "Material", :final => "Final"}
+    }
   end
 end
