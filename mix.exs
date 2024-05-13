a58b09063d89073e26c16eed912060652e174b56
friaca <friacajoao@gmail.com>
2024-05-13 00:54:05 -0300

    chore: adding Timex dep

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Cabinet.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:pbkdf2_elixir, "~> 2.2"}
+      {:pbkdf2_elixir, "~> 2.2"},
+      {:timex, "~> 3.0"}
     ]
   end
 
